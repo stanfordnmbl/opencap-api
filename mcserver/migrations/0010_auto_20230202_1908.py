d59ac02506789071c587bdc8b5db2301d4e00c04
Alberto <albertocasasortiz@gmail.com>
2023-03-29 14:56:18 -0700

    Revert "fixed conflicting migrations"

--- mcserver/migrations/0010_auto_20230202_1908.py
+++ mcserver/migrations/0010_auto_20230202_1908.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mcserver', '0010_auto_20230201_1502'),
+        ('mcserver', '0009_customemaildevice'),
     ]
 
     operations = [
