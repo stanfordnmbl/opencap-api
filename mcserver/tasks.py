951ecb5f0228cca6679cc9cd80d5f2463cf2e22d
Antoine Falisse <antoinefalisse@gmail.com>
2023-03-29 14:32:13 -0700

    roll back to 30 days and check every 2 and 4 hours

--- mcserver/tasks.py
+++ mcserver/tasks.py
@@ -4,30 +4,13 @@ from django.utils import timezone
 from datetime import timedelta
 
 
-# @shared_task
-# def cleanup_trashed_sessions():
-#     from .models import Session
-#     now = timezone.now()
-#     Session.objects.filter(
-#         trashed=True,
-#         trashed_at__lt=now-timedelta(days=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
-
-
-# @shared_task
-# def cleanup_trashed_trials():
-#     from .models import Trial
-#     now = timezone.now()
-#     Trial.objects.filter(
-#         trashed=True,
-#         trashed_at__lt=now-timedelta(days=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
-
 @shared_task
 def cleanup_trashed_sessions():
     from .models import Session
     now = timezone.now()
     Session.objects.filter(
         trashed=True,
-        trashed_at__lt=now-timedelta(minutes=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
+        trashed_at__lt=now-timedelta(days=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
 
 
 @shared_task
@@ -36,4 +19,4 @@ def cleanup_trashed_trials():
     now = timezone.now()
     Trial.objects.filter(
         trashed=True,
-        trashed_at__lt=now-timedelta(minutes=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
+        trashed_at__lt=now-timedelta(days=settings.TRASHED_OBJECTS_CLEANUP_DAYS)).delete()
