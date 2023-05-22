f9e63f12f533ddfd012ba2a747b59d049f93afa1
Anastasiia Didan <anastasiiadidan@planeks.net>
2023-05-22 15:07:51 +0300

    fix: ZipSession class with collect zip folder files methods, tests

--- mcserver/zipsession_v2.py
+++ mcserver/zipsession_v2.py
@@ -1,8 +1,15 @@
+import os
+import shutil
+import pickle
+
 from django.conf import settings
+
+from mcserver.models import Trial, Result
 
 
 class ZipSession():
     def __init__(
+        self,
         *args,
         delete_folder_after_zip=True,
         commit_zip_result=False,
@@ -10,12 +17,137 @@ class ZipSession():
     ):
         self.delete_folder_after_zip = delete_folder_after_zip
         self.commit_zip_result = commit_zip_result
+        self.readme_txt = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "data",
+            "README.txt"
+        )
 
-    def collect_session_data(self, session_id):
-        pass
+    def set_root_dir_path(self, root_dir_path):
+        self.root_dir_path = root_dir_path
+    
+    def get_root_dir_path(self):
+        return self.root_dir_path
+    
+    def collect_video_data(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        mapping_cam_device = {}
+        for idx, video in enumerate(trial.video_set.all().only("device_id", "video")):
+            video_root = os.path.join(
+                root_dir_path, "Videos", f"Cam{idx}", "InputMedia", trial.formated_name
+            )
+            os.makedirs(video_root, exist_ok=True)
+            shutil.copy2(
+                video.video.path, os.path.join(video_root, f"{trial.formated_name}.mov")
+            )
+            mapping_cam_device[str(video.device_id).replace('-', '').upper()] = idx
+
+        mapping_cam_device_path = os.path.join(root_dir_path, "Videos", 'mappingCamDevice.pickle')
+        if os.path.exists(mapping_cam_device_path):
+            with open(mapping_cam_device_path, "rb") as handle:
+                mapping_cam_device = {**mapping_cam_device, **pickle.load(handle)}
+        with open(mapping_cam_device_path, "wb") as handle:
+            pickle.dump(mapping_cam_device, handle)
+    
+    def collect_sync_videos(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        for result in trial.result_set.filter(tag="video-sync").only("media"):
+            video_root = os.path.join(
+                root_dir_path, "Videos", result.device_id, "InputMedia", trial.formated_name
+            )
+            os.makedirs(video_root, exist_ok=True)
+            ext = result.media.path.split(".")[-1]
+            shutil.copy2(
+                result.media.path,
+                os.path.join(video_root, f"{trial.formated_name}_sync.{ext}")
+            )
+    
+    def collect_pose_pickles(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        for result in trial.result_set.filter(tag="pose_pickle").only("device_id", "media"):
+            device_pickle_root = os.path.join(root_dir_path, "Videos", result.device_id, "OutputPkl")
+            os.makedirs(device_pickle_root, exist_ok=True)
+            shutil.copy2(
+                result.media.path,
+                os.path.join(device_pickle_root, f"{trial.formated_name}_keypoints.pkl")
+            )
+    
+    def collect_marker_data(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        for result in trial.result_set.filter(tag="marker_data").only("media"):
+            marker_data_root = os.path.join(root_dir_path, "MarkerData")
+            os.makedirs(marker_data_root, exist_ok=True)
+            shutil.copy2(
+                result.media.path,
+                os.path.join(marker_data_root, f"{trial.formated_name}.trc")
+            )
+    
+    def collect_kinematics_data(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        for result in trial.result_set.filter(tag="ik_results").only("media"):
+            kinematics_root = os.path.join(root_dir_path, "OpenSimData", "Kinematics")
+            os.makedirs(kinematics_root, exist_ok=True)
+            shutil.copy2(
+                result.media.path,
+                os.path.join(kinematics_root, f"{trial.formated_name}.mot")
+            )
+
+    def collect_opensim_model_data(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        opensim_result = trial.result_set.filter(tag="opensim_model").first()
+        if opensim_result:
+            opensim_model_short_filename = opensim_result.media.path.split("-")[-1]
+            model_root = os.path.join(root_dir_path, "OpenSimData", "Model")
+            os.makedirs(model_root, exist_ok=True)
+            shutil.copy2(
+                opensim_result.media.path,
+                os.path.join(model_root, opensim_model_short_filename)
+            )
+
+            if "LaiArnold" in opensim_model_short_filename:
+                print("Download geometry...")
+    
+    def collect_docs(self, trial):
+        root_dir_path = self.get_root_dir_path()
+        session_metadata_result = Result.objects.filter(
+            trial_id=trial.id, tag="session_metadata"
+        ).first()
+        if session_metadata_result:
+            shutil.copy2(
+                session_metadata_result.media.path,
+                os.path.join(root_dir_path, "sessionMetadata.yml")
+            )
+
+        shutil.copy2(self.readme_txt, os.path.join(root_dir_path, "README.txt"))
+
+    def collect_session_data(self, session_id, root_dir_path=settings.MEDIA_ROOT):
+        session_path = os.path.join(settings.MEDIA_ROOT, f"OpenCapData_{session_id}")
+        self.set_root_dir_path(session_path)
+        
+        calibration_trial = Trial.get_calibration_obj_or_none(session_id)
+        if calibration_trial:
+            print("Calibration trial processing...")
+
+        neutral_and_dynamic_trials = Trial.objects.filter(
+            session_id=session_id
+        ).exclude(name="calibration")
+        for trial in neutral_and_dynamic_trials:
+            self.collect_video_data(trial)
+            self.collect_sync_videos(trial)
+            self.collect_marker_data(trial)
+            self.collect_pose_pickles(trial)
+            self.collect_kinematics_data(trial)
+        
+        neutral_trial = Trial.get_neutral_obj_or_none(session_id)
+        if neutral_trial:
+            self.collect_opensim_model_data(neutral_trial)
+            self.collect_docs(neutral_trial)
+
+        return session_path
+        # return self.zip(session_path)
 
     def collect_subject_data(self, subject_id):
         pass
 
-    def zip(self, root_path):
+    def zip(self, root_dir_path):
         pass
--- tests/test_zipsession.py
+++ tests/test_zipsession.py
@@ -1,11 +1,19 @@
 import uuid
+import os
+import shutil
+import tempfile
+import pickle
+import glob
 
-from django.test import TestCase
+from django.conf import settings
+from django.test import TestCase, override_settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from mcserver.models import User, Session, Trial, Result, Video
 from mcserver.zipsession_v2 import ZipSession
 from mcserver.zipsession import downloadAndZipSession
+
+_temp_media = tempfile.mkdtemp()
 
 
 class TrialTests(TestCase):
@@ -180,10 +188,16 @@ class TrialTests(TestCase):
         )
 
 
+@override_settings(
+    MEDIA_ROOT=_temp_media,
+    DEFAULT_FILE_STORAGE="django.core.files.storage.FileSystemStorage"
+)
 class TestZipSession(TestCase):
     def setUp(self):
         """ Full session configuration
         """
+        self.zip_session = ZipSession()
+        self.zip_session.set_root_dir_path(settings.MEDIA_ROOT)
         self.user = User.objects.create_user(
             username="johndoe",
             email="johndoe@email.com",
@@ -203,6 +217,31 @@ class TestZipSession(TestCase):
                 device_id="all",
                 media=SimpleUploadedFile(f"{trial_name}.trc", content=b"markerdata")
             )
+            # /OpenSimData/Kinematics
+            Result.objects.create(
+                trial=trial,
+                tag="ik_results",
+                device_id="all",
+                media=SimpleUploadedFile(f"{trial_name}.mot", content=b"ikresults")
+            )
+
+            # /OpenSimData/Model
+            if trial_name == "neutral":
+                Result.objects.create(
+                    trial=trial,
+                    tag="session_metadata",
+                    device_id="all",
+                    media=SimpleUploadedFile(
+                        "random_session_metadata.yml",
+                        content=b"metadata"
+                    )
+                )
+                Result.objects.create(
+                    trial=trial,
+                    tag="opensim_model",
+                    device_id="all",
+                    media=SimpleUploadedFile("randommodel-LaiArnold.osim", content=b"")
+                )
             
             for device_id in ["Cam0", "Cam1"]:
                 # /Videos/Cam*/OutputPkl
@@ -235,88 +274,246 @@ class TestZipSession(TestCase):
                     )
                 )
 
-                # /OpenSimData/Kinematics
-                if trial_name == "neutral":
-                    Result.objects.create(
-                        trial=trial,
-                        tag="session_metadata",
-                        device_id="all",
-                        media=SimpleUploadedFile(
-                            "random_session_metadata.yml",
-                            content=b"metadata"
-                        )
-                    )
-                else:
-                    Result.objects.create(
-                        trial=trial,
-                        tag="ik_results",
-                        device_id="all",
-                        media=SimpleUploadedFile(f"{trial_name}.mot", content=b"ikresults")
-                    )
-
-            # /CalibrationImages/
-            calibration_trial = Trial.objects.create(
-                session=self.session,
-                name="calibration",
-                meta={"calibration": {"Cam0": 0, "Cam1": 1}}
+    def setUpCalibration(self):
+        # /CalibrationImages/
+        calibration_trial = Trial.objects.create(
+            session=self.session,
+            name="calibration",
+            meta={"calibration": {"Cam0": 0, "Cam1": 1}}
+        )
+        for device_id in ["Cam0", "Cam1"]:
+            Result.objects.create(
+                trial=calibration_trial,
+                device_id=device_id,
+                tag="calibration-img",
+                media=SimpleUploadedFile(
+                    f"random-extrinsicCalib_{device_id}.jpg",
+                    content=b"extrinsicCalib"
+                )
             )
             Result.objects.create(
                 trial=calibration_trial,
-                tag="opensim_model",
-                device_id="all",
-                media=SimpleUploadedFile("LaiArnold.osim", content=b"")
+                device_id=f"{device_id}_altSoln",
+                tag="calibration-img",
+                media=SimpleUploadedFile(
+                    f"random-extrinsicCalib_{device_id}.jpg",
+                    content=b"extrinsicCalib"
+                )
             )
-            for device_id in ["Cam0", "Cam1"]:
+            Result.objects.create(
+                trial=calibration_trial,
+                tag="calibration_parameters",
+                device_id=device_id,
+                media=SimpleUploadedFile(
+                    f"{device_id}_cameraIntrinsicsExtrinsics.pickle",
+                    content=b"cameraintrinsicsextrinsicspickle"
+                )
+            )
+            Result.objects.create(
+                trial=calibration_trial,
+                tag="camera_mapping",
+                device_id="all",
+                media=SimpleUploadedFile(
+                    f"{device_id}_mappingCamDevice.pickle",
+                    content=b"mappinCamDevice",
+                )
+            )
+            for opt in ["soln0", "soln1"]:
                 Result.objects.create(
                     trial=calibration_trial,
-                    device_id=device_id,
-                    tag="calibration-img",
+                    tag="calibration_parameters_options",
+                    device_id=f"{device_id}_{opt}",
                     media=SimpleUploadedFile(
-                        f"random-extrinsicCalib_{device_id}.jpg"
+                        f"{device_id}-cameraintrinsicsExtrinsics_{opt}.pickle",
+                        content=b"intrisicsExtrisics"
                     )
                 )
-                Result.objects.create(
-                    trial=calibration_trial,
-                    device_id=f"{device_id}_altSoln",
-                    tag="calobration-img",
-                    media=SimpleUploadedFile(
-                        f"random-extrinsicCalib_{device_id}.jpg"
-                    )
-                )
-                Result.objects.create(
-                    trial=calibration_trial,
-                    tag="calibration_parameters",
-                    device_id=device_id,
-                    media=SimpleUploadedFile(
-                        f"{device_id}_cameraIntrinsicsExtrinsics.pickle",
-                        content=b"cameraintrinsicsextrinsicspickle"
-                    )
-                )
-                Result.objects.create(
-                    trial=calibration_trial,
-                    tag="camera_mapping",
-                    device_id="all",
-                    media=SimpleUploadedFile(
-                        f"{device_id}_mappingCamDevice.pickle",
-                        content=b"mappinCamDevice",
-                    )
-                )
-                for opt in ["soln0", "soln1"]:
-                    Result.objects.create(
-                        trial=calibration_trial,
-                        tag="calibration_parameters_options",
-                        device_id=f"{device_id}_{opt}",
-                        media=SimpleUploadedFile(
-                            f"{device_id}-cameraintrinsicsExtrinsics_{opt}.pickle",
-                            content=b"intrisicsExtrisics"
-                        )
-                    )
 
-    def test_create_session_folder_with_correct_structure_and_name(self):
-        data = ZipSession().collect_session_data(self.session.id)
+    def test_collect_videos_for_neutral_trial(self):
+        neutral_trial = Trial.objects.get(name="neutral")
+        videos_folder = os.path.join(settings.MEDIA_ROOT, "Videos")
+        self.assertFalse(os.path.exists(videos_folder))
+        self.zip_session.collect_video_data(neutral_trial)
+        self.assertTrue(os.path.exists(videos_folder))
+        for idx in range(2):
+            trial_video_path = os.path.join(
+                videos_folder, f"Cam{idx}", "InputMedia", "neutral", "neutral.mov"
+            )
+            self.assertTrue(os.path.exists(trial_video_path))
+        
+        mapping_cam_device_path = os.path.join(videos_folder, "mappingCamDevice.pickle")
+        self.assertTrue(os.path.exists(mapping_cam_device_path))
+        with open(mapping_cam_device_path, 'rb') as handle:
+            mapping_cam_device = pickle.load(handle)
+            for idx, video in enumerate(neutral_trial.video_set.all().only("device_id")):
+                expected_device_key = str(video.device_id).replace('-', '').upper()
+                self.assertEqual(mapping_cam_device[expected_device_key], idx)
+    
+    def test_collect_videos_for_multiple_trials_with_correct_mapping_cam_device_overwriting(self):
+        videos_folder = os.path.join(settings.MEDIA_ROOT, "Videos")
+        self.assertFalse(os.path.exists(videos_folder))
+        for trial_name in self.trials[2:4]:
+            trial = Trial.objects.get(name=trial_name)
+            self.zip_session.collect_video_data(trial)
+            for idx in range(2):
+                trial_video_path = os.path.join(
+                    videos_folder,
+                    f"Cam{idx}",
+                    "InputMedia",
+                    trial.formated_name,
+                    f"{trial.formated_name}.mov"
+                )
+                self.assertTrue(os.path.exists(trial_video_path))
 
-    def test_zip_session_folder_successful(self):
+        mapping_cam_device_path = os.path.join(videos_folder, "mappingCamDevice.pickle")
+        self.assertTrue(os.path.exists(mapping_cam_device_path))
+        with open(mapping_cam_device_path, 'rb') as handle:
+            mapping_cam_device = pickle.load(handle)
+            for trial in Trial.objects.filter(name__in=self.trials[2:4]):
+                for idx, video in enumerate(trial.video_set.only("device_id")):
+                    expected_device_key = str(video.device_id).replace('-', '').upper()
+                    self.assertEqual(mapping_cam_device[expected_device_key], idx)
+    
+    def test_collect_sync_videos_for_trial(self):
+        videos_folder = os.path.join(settings.MEDIA_ROOT, "Videos")
+        self.assertFalse(os.path.exists(videos_folder))
+        trial = Trial.objects.first()
+        self.zip_session.collect_sync_videos(trial)
+        self.assertTrue(os.path.exists(videos_folder))
+        for idx in range(2):
+            trial_sync_video_path = os.path.join(
+                videos_folder,
+                f"Cam{idx}",
+                "InputMedia",
+                trial.formated_name,
+                f"{trial.formated_name}_sync.mp4"
+            )
+            self.assertTrue(os.path.exists(trial_sync_video_path))
+    
+    def test_collect_pose_pickle_files_for_trial(self):
+        videos_folder = os.path.join(settings.MEDIA_ROOT, "Videos")
+        self.assertFalse(os.path.exists(videos_folder))
+        trial = Trial.objects.first()
+        self.zip_session.collect_pose_pickles(trial)
+        self.assertTrue(os.path.exists(videos_folder))
+        for idx in range(2):
+            trial_pose_pickle_path = os.path.join(
+                videos_folder,
+                f"Cam{idx}",
+                "OutputPkl",
+                f"{trial.formated_name}_keypoints.pkl"
+            )
+            self.assertTrue(os.path.exists(trial_pose_pickle_path))
+            with open(trial_pose_pickle_path, "rb") as pose_pickle:
+                self.assertEqual(pose_pickle.read(), b"posepickle")
+    
+    def test_collect_marker_data_files_for_trial(self):
+        marker_data_folder = os.path.join(settings.MEDIA_ROOT, "MarkerData")
+        self.assertFalse(os.path.exists(marker_data_folder))
+        trial = Trial.objects.first()
+        self.zip_session.collect_marker_data(trial)
+        self.assertTrue(os.path.exists(marker_data_folder))
+        expected_marker_data_file_path = os.path.join(
+            marker_data_folder, f"{trial.formated_name}.trc"
+        )
+        self.assertTrue(os.path.exists(expected_marker_data_file_path))
+        with open(expected_marker_data_file_path, "rb") as marker_data:
+            self.assertEqual(marker_data.read(), b"markerdata")
+    
+    def test_collect_kinematics_data_files_for_trial(self):
+        kinematics_folder = os.path.join(
+            settings.MEDIA_ROOT, "OpenSimData", "Kinematics"
+        )
+        self.assertFalse(os.path.exists(kinematics_folder))
+        trial = Trial.objects.first()
+        self.zip_session.collect_kinematics_data(trial)
+        self.assertTrue(os.path.exists(kinematics_folder))
+        expected_kinematics_file_path = os.path.join(
+            kinematics_folder, f"{trial.formated_name}.mot"
+        )
+        self.assertTrue(os.path.exists(expected_kinematics_file_path))
+        with open(expected_kinematics_file_path, "rb") as kinematics_data:
+            self.assertEqual(kinematics_data.read(), b"ikresults")
+    
+    def test_collect_opensim_model_file_for_neutral_trial(self):
+        trial = Trial.objects.get(name="neutral")
+        opensim_model_folder = os.path.join(
+            settings.MEDIA_ROOT, "OpenSimData", "Model"
+        )
+        self.assertFalse(os.path.exists(opensim_model_folder))
+        self.zip_session.collect_opensim_model_data(trial)
+        self.assertTrue(opensim_model_folder)
+        expected_opensim_model_file_path = os.path.join(
+            opensim_model_folder, "LaiArnold.osim"
+        )
+        self.assertTrue(os.path.exists(expected_opensim_model_file_path))
+
+    def test_collect_opensim_model_skips_data_upload_if_trial_has_no_opensim_result(self):
+        trial = Trial.objects.get(name="jump")
+        self.assertFalse(
+            trial.result_set.filter(tag="opensim_model").exists()
+        )
+        opensim_model_folder = os.path.join(
+            settings.MEDIA_ROOT, "OpenSimData", "Model"
+        )
+        self.assertFalse(os.path.exists(opensim_model_folder))
+        self.zip_session.collect_opensim_model_data(trial)
+        self.assertFalse(os.path.exists(opensim_model_folder))
+    
+    def test_collect_docs(self):
+        trial = Trial.objects.get(name="neutral")
+        session_metadata_yml_path = os.path.join(settings.MEDIA_ROOT, "sessionMetadata.yml")
+        readme_txt_path = os.path.join(settings.MEDIA_ROOT, "README.txt")
+        self.assertFalse(os.path.exists(session_metadata_yml_path))
+        self.assertFalse(os.path.exists(readme_txt_path))
+        zip_session =self.zip_session
+        zip_session.collect_docs(trial)
+        self.assertTrue(os.path.exists(session_metadata_yml_path))
+        self.assertTrue(os.path.exists(readme_txt_path))
+
+        with open(zip_session.readme_txt, "rb") as src, open(readme_txt_path, "rb") as dist:
+            self.assertEqual(dist.read(), src.read())
+
+    def test_create_session_dir_with_correct_structure_and_name(self):
+        session_dir = os.path.join(
+            settings.MEDIA_ROOT, f"OpenCapData_{self.session.id}"
+        )
+        res_session_dir = self.zip_session.collect_session_data(self.session.id)
+        self.assertEqual(res_session_dir, session_dir)
+        self.assertTrue(os.path.exists(session_dir))
+        for root, dirs, files in os.walk(session_dir):
+            if root == session_dir:
+                self.assertEqual(set(dirs), {"Videos", "OpenSimData", "MarkerData"})
+                self.assertEqual(set(files), {"sessionMetadata.yml", "README.txt"})
+            
+            if root == os.path.join(session_dir, "Videos"):
+                self.assertEqual(set(dirs), {"Cam0", "Cam1"})
+                self.assertEqual(set(files), {"mappingCamDevice.pickle"})
+            
+            if (
+                root == os.path.join(session_dir, "Videos", "Cam0")
+                or root == os.path.join(session_dir, "Videos", "Cam1")
+            ):
+                self.assertEqual(set(dirs), {"OutputPkl", "InputMedia"})
+                # self.assertEqual(set(files), {"cameraIntrinsicsExtrinsics.pickle"})
+            
+            if root == os.path.join(session_dir, "OpenSimData"):
+                self.assertEqual(set(dirs), {"Kinematics", "Model"})
+                self.assertEqual(set(files), set())
+            
+            if root == os.path.join(session_dir, "OpenSimData", "Kinematics"):
+                self.assertEqual(set(dirs), set())
+                self.assertEqual(set(files), {f"{name}.mot" for name in self.trials})
+            
+            if root == os.path.join(session_dir, "OpenSimData", "Model"):
+                self.assertEqual(set(dirs), set())
+                self.assertEqual(set(files), {"LaiArnold.osim"})
+
+    def test_zip_session_dir_successful(self):
         pass
 
     def test_zip_subject_folder_successful(self):
         pass
+
+    def tearDown(self):
+        shutil.rmtree(settings.MEDIA_ROOT)
