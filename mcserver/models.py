8b372c96440062fc5a07f03f812461710674b1dc
Seeeeeyo <selimgilon@gmail.com>
2025-06-06 10:31:27 -0600

    Added isMono field in DB + logic behind it for dequeue and n cameras

--- mcserver/models.py
+++ mcserver/models.py
@@ -78,6 +78,8 @@ class Session(models.Model):
 
     trashed = models.BooleanField(default=False)
     trashed_at = models.DateTimeField(blank=True, null=True)
+    
+    isMono = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-created_at']
